4507dc152f83a9a751b2b919b4e9416a1a4562b5
saidsoud72 <said.omar@student.moringaschool.com>
2022-11-07 11:49:53 +0300

    Final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,11 @@ puts "Creating reviews..."
 # * TODO: create reviews! Remember, a review belongs to a product *
 # * and a review belongs to an user.                              *
 # *****************************************************************
-# Create Reviews Here
+
+stapler = Review.create(star_rating: 5, comment: "Works great. I'll try erase my past with it!", product_id: product3.id, user_id: user3.id)
+stapler = Review.create(star_rating: 4, comment: "BonsaiCF's Staplers are the best", product_id: product1.id, user_id: user1.id)
+whiteboard = Review.create(star_rating: 3, comment: "Cleans up well!", product_id: product2.id, user_id: user1.id)
+tape = Review.create(star_rating: 1, comment: "Not what I ordered!", product_id: product5.id, user_id: user2.id)
+stapler2 = Review.create(star_rating: 5, comment: "Most satisfying stapler. Wow!", product_id: product1.id, user_id: user2.id)
 
 puts "Seeding done!"
